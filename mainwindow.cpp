c3ab9b970385dadf6cd7a25fe8d03565067f80d4
xewhysis <xewhysis14@gmail.com>
2026-04-18 18:17:46 +0300

    написан доп файл turingmachine.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,50 @@
-#include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "turingmachine.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+void TuringMachine::reset() {
+    tape.clear();
+    rules.clear();
+    head = 0;
+    currentState = "q0";
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+void TuringMachine::setBlank(QChar b) {
+    blank = b;
+}
+
+void TuringMachine::setTape(const QString &input) {
+    tape.clear();
+    for (int i = 0; i < input.size(); i++)
+        tape[i] = input[i];
+}
+
+void TuringMachine::setRule(const QString &state, QChar symbol, const Rule &rule) {
+    rules[state][symbol] = rule;
+}
+
+bool TuringMachine::step() {
+    QChar current = tape.contains(head) ? tape[head] : blank;
+
+    if (!rules.contains(currentState) ||
+        !rules[currentState].contains(current))
+        return false;
+
+    Rule r = rules[currentState][current];
+
+    tape[head] = r.write;
+    head += r.move;
+    currentState = r.nextState;
+
+    return true;
+}
+
+QString TuringMachine::getState() const {
+    return currentState;
+}
+
+int TuringMachine::getHead() const {
+    return head;
+}
+
+QChar TuringMachine::getSymbol(int pos) const {
+    return tape.contains(pos) ? tape[pos] : blank;
 }
